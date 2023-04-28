16cf693d13f85e38d5c6e42fce87211b92a02b7f
FusTaFah <mfadel_1990@hotmail.com>
2023-04-28 18:08:23 +0100

    fixes issue with player input handling

--- Assets/Scripts/ChefBehaviour.cs
+++ Assets/Scripts/ChefBehaviour.cs
@@ -2,21 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.Netcode;
+using UnityEngine.InputSystem;
 
 public class ChefBehaviour : NetworkBehaviour
 {
+    //[SerializeField]
+    //private InputAction MoveVerticalAction;
+    private Rigidbody playerBody;
+    private PlayerInput playerInput;
+
+
+    private void Start()
+    {
+        playerBody = gameObject.GetComponent<Rigidbody>();
+        playerInput = gameObject.GetComponent<PlayerInput>();
+        playerInput.enabled = true;
+    }
+
     [ServerRpc]
     void SubmitPositionRequestServerRpc(ServerRpcParams serverRpcParams = default)
     {
 
     }
 
-    public void OnMove()
+    public void OnMove(InputAction.CallbackContext callbackContext)
     {
-        
+        Vector2 movementDirection = callbackContext.ReadValue<Vector2>();
+        if (IsClient)
+        {
+
+        }
+        else
+        {
+            playerBody.position += new Vector3(movementDirection.x, 0.0f, movementDirection.y);
+        }
     }
 
-
+    public void OnThrow(InputAction.CallbackContext callbackContext)
+    {
+        Debug.Log("test throw");
+    }
 
     void Update()
     {
